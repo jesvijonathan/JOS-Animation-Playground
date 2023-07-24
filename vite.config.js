179d02aff101a39505c532acb6d6f6f24161edb5
Jesvi Jonathan <jesvi22j@gmail.com>
2023-07-24 09:17:24 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
+// add github pages base
+
 export default defineConfig({
   plugins: [vue()],
+  base: "/JOS-Animation-Playground/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -13,7 +15,12 @@ export default defineConfig({
   },
 });
 
-module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/JOS-Animation-Playground/" : "/",
-};
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       "@": fileURLToPath(new URL("./src", import.meta.url)),
+//     },
+//   },
+// });
