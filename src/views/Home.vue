3cf344390750e25f04a05213394129d57dbbc151
Jesvi Jonathan <jesvi22j@gmail.com>
2023-07-30 00:42:44 +0530

    added landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+//"@/assets/floating_websites/web1"
+</script>
 
 <template>
   <section id="home">
@@ -36,25 +38,148 @@
       </div>
     </div>
     <div class="home_showcase">
-      <div class="showcase jos"></div>
+      <div class="showcase jos">
+        <div class="showcase_it piece_1"></div>
+
+        <div class="showcase_it piece_2"></div>
+
+        <div class="showcase_it piece_3"></div>
+
+        <div class="showcase_it piece_4"></div>
+
+        <div class="showcase_it piece_5"></div>
+
+        <div
+          class="showcase_it piece_1 piece_7"
+          style="animation-delay: 5s; bottom: -80%"
+        ></div>
+
+        <div
+          class="showcase_it piece_2 piece_6"
+          style="animation-delay: 5s; bottom: -40%; left: 10%; width: 14vw"
+        ></div>
+      </div>
     </div>
   </section>
 </template>
 
 <style scoped>
+.piece_1 {
+  width: 21vw;
+  height: 14vw;
+  bottom: -20%;
+  left: 0%;
+  background-image: url("@/assets/floating_websites/web8.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  /* transform: rotateY(10deg) rotateX(40deg); */
+
+  animation: showcase linear forwards 15s infinite;
+}
+.piece_2 {
+  width: 18vw;
+  height: 11vw;
+  bottom: -20%;
+  left: 40%;
+  background-color: #9c9c9c;
+  background-image: url("@/assets/floating_websites/web2.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  transform: rotateY(0deg) rotateX(-40deg);
+
+  animation: showcase linear forwards 15s 1s infinite;
+}
+.piece_3 {
+  width: 28vw;
+  height: 17vw;
+  bottom: -50%;
+  left: 20%;
+  background-color: #464646;
+  background-image: url("@/assets/floating_websites/web6.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  transform: rotateY(0deg) rotateX(-40deg);
+
+  animation: showcase linear forwards 15s 2s infinite;
+}
+.piece_4 {
+  width: 25vw;
+  height: 20vw;
+  bottom: -100%;
+  left: 30%;
+  z-index: 1;
+  background-color: #9c9c9c;
+  background-image: url("@/assets/floating_websites/web5.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  transform: rotateY(0deg) rotateX(-40deg);
+
+  animation: showcase linear forwards 15s 3s infinite;
+}
+.piece_5 {
+  width: 25vw;
+  height: 17vw;
+  bottom: -135%;
+  left: 20%;
+  background-image: url("@/assets/floating_websites/web4.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  transform: rotateY(0deg) rotateX(-40deg);
+
+  transition: 0.7s ease;
+  animation: showcase linear forwards 15s 4s infinite;
+}
+.piece_6 {
+  height: 10vw;
+  width: 42vw;
+  background-image: url("@/assets/floating_websites/web3.png");
+}
+.piece_7 {
+  background-image: url("@/assets/floating_websites/web7.png");
+}
+.showcase_it {
+  position: absolute;
+  /* box shadow */
+  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.3);
+  border: 0.1vw solid rgb(0, 0, 0, 0.5);
+  /* animation-timing-function: cubic-bezier(0.445, 0.05, 0.55, 1); */
+  animation-duration: 15s;
+}
+/* scroll animation */
+@keyframes showcase {
+  0% {
+    opacity: 0;
+  }
+  4% {
+    opacity: 1;
+  }
+
+  90% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+    transform: translateY(-100vw);
+  }
+}
 .home_showcase {
   position: absolute;
   overflow: hidden;
   top: 0;
   right: 0;
   top: 50%;
-  margin-right: 10vw;
+  margin-right: 5vw;
   transform: translateY(-50%);
 }
 .showcase {
-  width: 35vw;
+  width: 40vw;
   height: 90vh;
-  border: 2px solid var(--color-primary);
+  border: 0px solid var(--color-primary);
 }
 .home_but {
   display: flex;
